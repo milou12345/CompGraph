a452a41dea36c45fddcc3c9d756a63bc75951e05
Klaus <klaus.juenemann@haw-hamburg.de>
2019-11-08 17:19:42 +0100

    Rolling motion added

--- RollingBall.js
+++ RollingBall.js
@@ -37,10 +37,16 @@ const ballGeo = new THREE.SphereGeometry(ballRadius, 8, 8);
 const ball = new THREE.Mesh(ballGeo,  new THREE.MeshBasicMaterial( {color: 0x0000ff,
                                                                     wireframeLinewidth:2,
                                                                     wireframe:true}));
+ball.matrixAutoUpdate = false;
 scene.add(ball);
+
+// speed and current position of translational motion
 let ballSpeed = new THREE.Vector3(5*Math.random(), 0, 5*Math.random());
 let ballPos = new THREE.Vector3(0, ballRadius, 0);
 
+// axis and angular velocity of rotational motion
+let ax = new THREE.Vector3(0,1,0).cross(ballSpeed).normalize();
+let omega = ballSpeed.length() / ballRadius;
 
 //* Render loop
 const computerClock = new THREE.Clock();
@@ -48,17 +54,30 @@ const controls = new THREE.TrackballControls( camera );
 function render() {
   requestAnimationFrame(render);
 
-  // translational motion of ball
   const dt = computerClock.getDelta();
-  ballPos.add(ballSpeed.clone().multiplyScalar(dt));
-  ball.position.copy(ballPos);
+  const t = computerClock.getElapsedTime();
 
-  // Implement reflection
+  // update position of ball:
+  ballPos.add(ballSpeed.clone().multiplyScalar(dt));
+
+  // dR: incremental rotation matrix that performs the rotation of the current time step.
+  const dR = new THREE.Matrix4();
+  dR.makeRotationAxis(ax, omega*dt);
+  // multiply with dR form the left (matrix.multiply multiplies from the right!)
+  ball.matrix.premultiply(dR);
+  // set translational part of matrix to current position:
+  ball.matrix.setPosition(ballPos);
+
+  // Implement reflection: the axis of rotation has to be updated since the direction of the speed changes!
   if(ballPos.x> floorX/2) {
     ballSpeed.x = - Math.abs(ballSpeed.x);
+    ax = new THREE.Vector3(0,1,0).cross(ballSpeed).normalize();
+    omega = ballSpeed.length() / ballRadius;
   }
   if(ballPos.z > floorZ/2) {
     ballSpeed.z = - Math.abs(ballSpeed.z);
+    ax = new THREE.Vector3(0,1,0).cross(ballSpeed).normalize();
+    omega = ballSpeed.length() / ballRadius;
   }
 
   controls.update();
