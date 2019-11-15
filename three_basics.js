eca5ddc152b58724aa546f57cae54e14ee67edc7
Klaus <klaus.juenemann@haw-hamburg.de>
2019-11-15 07:14:57 +0100

    Code for lecture 7

--- three_basics.js
+++ three_basics.js
@@ -9,7 +9,8 @@ const camera = new THREE.PerspectiveCamera( 75, canvas.width / canvas.height, 0.
 camera.position.set(0,0,3);
 camera.lookAt(scene.position);   // camera looks at origin
 
-const geo = new THREE.SphereGeometry(0.2, 16, 16);
+const radius = 0.2;
+const geo = new THREE.SphereGeometry(radius, 16, 16);
 
 const params = {color: "green",
                 wireframe:true,
@@ -20,6 +21,7 @@ const mat = new THREE.MeshBasicMaterial(params);
 // Combine geometry and material to a new object:
 const obj = new THREE.Mesh(geo, mat);
 scene.add(obj);
+const speed = new THREE.Vector3(-3,0,0);
 
 // Draw everything
 const axes = new THREE.AxesHelper();
@@ -29,7 +31,11 @@ const clock = new THREE.Clock();
 function render() {
   requestAnimationFrame(render);
 
+  const h = clock.getDelta();
   const t = clock.getElapsedTime();
+
+  // move ball
+  obj.position.add(speed.clone().multiplyScalar(h));
 
   controls.update();
   renderer.render(scene, camera);
